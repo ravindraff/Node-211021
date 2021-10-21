9d6884a0193b0f3aac07079cadc38e61b12448eb
ravindraff <ravindra.m568@gmail.com>
2021-10-21 15:15:11 +0530

    post files added

---
+++ postparameters/server.js
@@ -0,0 +1,33 @@
+"use strict";
+exports.__esModule = true;
+var express = require("express");
+var bodyParser = require("body-parser"); //for typscript code only, use require for js
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+var auth = function (req, res, next) {
+    var allHeaders = req.headers;
+    var token = allHeaders.token;
+    if (token === "nodejs") {
+        next();
+    }
+    else {
+        res.status(500).json({
+            auth: "Authentication failed"
+        });
+    }
+};
+app.post('/login', [auth], function (req, res) {
+    console.log(req.body);
+    var uname = req.body.uname;
+    var upwd = req.body.upwd;
+    if (uname === "admin" && upwd === "admin") {
+        res.status(200).json({ message: "Login Successfully" });
+    }
+    else {
+        res.status(401).json({ message: "Login Failure" });
+    }
+});
+app.listen(8070, function () {
+    console.log("server started successfully on port ");
+});
